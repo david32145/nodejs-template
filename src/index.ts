4f7e51eea8ecc76f2b2972571e70d35e8080c340
david32145 <nascimento32145@gmail.com>
2020-06-21 15:22:37 -0300

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('here')
+import app from './app'
+
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, function(...err:any[]) {
+  if(err && err.length) {
+    console.log('An error ocurred')
+    console.error(err)
+  } else {
+    console.log(`Server is running in http://localhost${PORT}`)
+  }
+})
